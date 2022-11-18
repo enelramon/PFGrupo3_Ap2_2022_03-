edfc0e8ebdb9cc1e950f147e59514a7974674c1e
pablouix <pablouix@gmail.com>
2022-11-18 15:02:45 -0400

    ui categorias responsiva

--- EmpleosDoApp/app/src/main/java/com/ucne/empleosdoapp/ui/categoria/CategoriaScreen.kt
+++ EmpleosDoApp/app/src/main/java/com/ucne/empleosdoapp/ui/categoria/CategoriaScreen.kt
@@ -1,14 +1,19 @@
 package com.ucne.empleosdoapp.ui.categoria
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ucne.empleosdoapp.R
 
 @Composable
 fun CategoriaScreen(
@@ -24,15 +29,16 @@ fun CategoriaScreen(
                         verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.Center
                     ) {
-                        Text(text = "Categoria")
+                        Text(text = "Categoría", fontSize = 16.sp)
                     }
+
                 },
-                elevation = 20.dp,
+                elevation = 2.dp,
                 backgroundColor = Color.White
             )
         }
     ) {
-        Column(
+       /* Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
@@ -44,5 +50,99 @@ fun CategoriaScreen(
                 
             }
         }
+
+
+        */
+        LazyVerticalGrid(
+            GridCells.Adaptive(minSize = 110.dp),
+            modifier = Modifier.padding(2.dp).fillMaxSize()
+        ){
+            item {
+                Card(modifier = Modifier
+                    .fillMaxSize()
+                    .height(160.dp)
+                    .padding(4.dp,5.dp),
+                    elevation = 5.dp
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.desarrollo),
+                            contentDescription = null,
+                            tint = Color(0xFF8E1DFF)
+                        )
+                        Text(text = "Desarrollo")
+                    }
+                }
+            }
+
+            item {
+                Card(modifier = Modifier
+                    .fillMaxSize()
+                    .height(160.dp)
+                    .padding(4.dp,5.dp),
+                    elevation = 5.dp,
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.basededatos),
+                            contentDescription = null,
+                            tint = Color(0xFF8E1DFF)
+                        )
+                        Text(text = "Base de Datos")
+                    }
+                }
+            }
+
+            item {
+                Card(modifier = Modifier
+                    .fillMaxSize()
+                    .height(160.dp)
+                    .padding(4.dp,5.dp),
+                    elevation = 5.dp,
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.seguridad),
+                            contentDescription = null,
+                            tint = Color(0xFF8E1DFF)
+                        )
+                        Text(text = "Seguridad")
+                    }
+                }
+            }
+
+            item {
+                Card(modifier = Modifier
+                    .fillMaxSize()
+                    .height(160.dp)
+                    .padding(4.dp,5.dp),
+                    elevation = 5.dp,
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.diseno),
+                            contentDescription = null,
+                            tint = Color(0xFF8E1DFF)
+                        )
+                        Text(text = "Diseño UI/UX")
+                    }
+                }
+            }
+
+        }
+
     }
 }
