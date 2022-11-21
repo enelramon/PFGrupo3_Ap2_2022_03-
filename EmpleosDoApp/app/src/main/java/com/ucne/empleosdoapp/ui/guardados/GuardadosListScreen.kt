41cf433d128aeab041e23de9d001f06f4da677e3
pablouix <pablouix@gmail.com>
2022-11-21 14:52:00 -0400

    Card de Guardados y select categoria

--- EmpleosDoApp/app/src/main/java/com/ucne/empleosdoapp/ui/guardados/GuardadosListScreen.kt
+++ EmpleosDoApp/app/src/main/java/com/ucne/empleosdoapp/ui/guardados/GuardadosListScreen.kt
@@ -1,48 +1,131 @@
 package com.ucne.empleosdoapp.ui.guardados
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ucne.empleosdoapp.R
 
 @Composable
 fun GuardadosListScreen(
     onClickSelected: () -> Unit
 ) {
-    Scaffold (
-        topBar =  {
-            TopAppBar(
-                title = {
-                    Row(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        Text(text = "Guardados")
-                    }
-                },
-                elevation = 20.dp,
-                backgroundColor = Color.White
-            )
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text("Desarrollo/Guardados", fontSize = 16.sp) }, modifier = Modifier.shadow(8.dp))
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(8.dp),) {
+                Text(text = "Back")
+            }
+
         },
-        modifier = Modifier.fillMaxSize(),
-        backgroundColor = MaterialTheme.colors.background
     ){
-        Column(
+        LazyColumn(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.Top
+
         ) {
-            Text("Prueba")
+
+            item {
+                Card(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(116.dp)
+                    .padding(4.dp, 0.dp), elevation = 2.dp) {
+                    Row() {
+                        Card(modifier = Modifier
+                            .fillMaxHeight()
+                            .width(116.dp)) {
+                            Icon(
+                                painter = painterResource(R.drawable.logo),
+                                contentDescription = null,
+                                tint = Color.Blue, modifier = Modifier
+                                    .size(121.dp, 51.dp) /* tamano de logo */
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Column(modifier = Modifier.padding(5.dp)) {
+                            Text(text = "Consultor de Implementacion y producto")
+                            Text(text = "WWT Company")
+                            Text(text = "Presencial")
+
+                            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                                Column() {
+                                    Text(text = "Tiempo completo")
+                                    Text(text = "Santiago - 21/11/2022")
+                                }
+                                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(8.dp),) {
+
+                                    Icon(
+                                        painter = painterResource(R.drawable.guardado),
+                                        contentDescription = null,
+                                        tint = Color.White
+                                    )
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            /**/
+
+            item {
+                Card(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(116.dp)
+                    .padding(4.dp, 0.dp), elevation = 2.dp) {
+                    Row() {
+                        Card(modifier = Modifier
+                            .fillMaxHeight()
+                            .width(116.dp)) {
+                            Icon(
+                                painter = painterResource(R.drawable.logo),
+                                contentDescription = null,
+                                tint = Color.Blue, modifier = Modifier
+                                    .size(121.dp, 51.dp) /* tamano de logo */
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Column(modifier = Modifier.padding(5.dp)) {
+                            Text(text = "Consultor de Implementacion y producto")
+                            Text(text = "WWT Company")
+                            Text(text = "Presencial")
+
+                            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                                Column() {
+                                    Text(text = "Tiempo completo")
+                                    Text(text = "Santiago - 21/11/2022")
+                                }
+                                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(8.dp),) {
+
+                                    Icon(
+                                        painter = painterResource(R.drawable.guardado),
+                                        contentDescription = null,
+                                        tint = Color.White
+                                    )
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+            }
+
+
         }
     }
 }
