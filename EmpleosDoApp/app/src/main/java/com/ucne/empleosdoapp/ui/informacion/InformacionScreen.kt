0621e3865d4435f81625f4a46e6800a27e549163
pablouix <pablouix@gmail.com>
2022-11-23 15:21:14 -0400

    modificando ui info

--- EmpleosDoApp/app/src/main/java/com/ucne/empleosdoapp/ui/informacion/InformacionScreen.kt
+++ EmpleosDoApp/app/src/main/java/com/ucne/empleosdoapp/ui/informacion/InformacionScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
@@ -66,7 +67,8 @@ private fun Inicio() {
                 Image(
                     modifier = Modifier.size(200.dp),
                     painter = painterResource(id = R.drawable.infocomputer),
-                    contentDescription = null
+                    contentDescription = null,
+
                 )
             }
         }
@@ -86,16 +88,19 @@ private fun CardTop() {
         Image(
             painter = painterResource(id = R.drawable.fondoinfo),
             modifier = Modifier.fillMaxSize(),
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop
         )
         Row(
             modifier = Modifier.fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Spacer(modifier = Modifier.width(5.dp))
             Iconos(sizeIcon = 120.dp, idIcon = R.drawable.logo)
-            Spacer(modifier = Modifier.width(20.dp))
+            Spacer(modifier = Modifier.width(10.dp))
             BotonValorar()
+            Spacer(modifier = Modifier.width(5.dp))
         }
     }
 }
@@ -129,10 +134,9 @@ private fun Iconos(sizeIcon: Dp, idIcon: Int) {
 @Composable
 private fun TextoInfo(){
     Column(modifier = Modifier.padding(4.dp, 0.dp)) {
-        Text(text = "¿Qué es Empleos Do?\n", fontWeight = FontWeight.Black)
-        Text(text = "   Es una app que permite ver empleos relacionados al área de tecnología, disponibles en república dominicana.\n")
-        Text(text = "   Accede a una gran cantidad de disponibilidad de empleos más recientes en república dominicana y aplicar a ellos de una forma fácil y sencilla.\n")
-        Text(text = "   También conoces como es la forma de trabajar de estas empresas, ubicación, requisitos, sueldos.\n")
-        Text(text = "   Encontraras todas las informaciones necesarias para entrar al mercado laboral. \n")
+        Text(text = "¿Qué es Empleos Do?\n", fontWeight = FontWeight.Black, fontSize = 16.sp)
+        Text(text = "Es una app que permite ver empleos relacionados al área de tecnología, disponibles en república dominicana.\n", fontSize = 14.sp)
+        Text(text = "Accede a una gran cantidad de disponibilidad de empleos más recientes en república dominicana y aplica a ellos de una forma fácil y sencilla.\n", fontSize = 14.sp)
+        Text(text = "También conoces como es la forma de trabajar de estas empresas, ubicación, requisitos, sueldos, entre otros. Encontraras todas las informaciones necesarias para entrar al mercado laboral.\n", fontSize = 14.sp)
     }
 }
