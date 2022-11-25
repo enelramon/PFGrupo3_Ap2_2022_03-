929bba9380cc883f6d7126f3e98868ab703d1ba4
pablouix <pablouix@gmail.com>
2022-11-25 17:58:20 -0400

    Poniendo imagen a card

--- EmpleosDoApp/app/src/main/java/com/ucne/empleosdoapp/ui/categoria_list/CategoriaListScreen.kt
+++ EmpleosDoApp/app/src/main/java/com/ucne/empleosdoapp/ui/categoria_list/CategoriaListScreen.kt
@@ -1,21 +1,28 @@
 package com.ucne.empleosdoapp.ui.categoria_list
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.ucne.empleosdoapp.R
 import com.ucne.empleosdoapp.data.remote.dto.EmpleoDto
 import com.ucne.empleosdoapp.ui.theme.ColorPri
@@ -110,8 +117,18 @@ private fun SelectedCard(
                     modifier = Modifier
                         .fillMaxHeight()
                         .width(130.dp),
-                    backgroundColor = Color.Blue
-                ) {}
+
+                ) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(empleo.logoEmpresa)
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
                 Spacer(modifier = Modifier.padding(3.dp, 0.dp))
                 Column(
                     modifier = Modifier
